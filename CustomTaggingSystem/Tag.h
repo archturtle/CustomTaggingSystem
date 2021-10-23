634a991870c95d7ba6e614cfd7564a3728dd6764
Siddhartha Juluru <31663833+archturtle@users.noreply.github.com>
2021-10-23 18:10:41 -0400

    Added comments to Tag.h

--- CustomTaggingSystem/Tag.h
+++ CustomTaggingSystem/Tag.h
@@ -12,16 +12,37 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface Tag : NSObject
 
+/**
+ The ID of the tag.
+ */
 @property (readonly) NSString * ID;
 
+/**
+ The name the tag should display when added.
+ */
 @property (readonly) NSString * name;
 
+/**
+ The color the tag should display when added.
+ */
 @property (readonly) NSColor * color;
 
+/**
+ The number of items the tag is assigned to.
+ */
 @property (readonly) int items;
 
 - (instancetype) init __attribute__((unavailable("init not available, use initWithName:Color:")));
 
+/**
+ A convienince initializer to create a new tag. In this initializer, the ID
+ and the number of items are randomly generated.
+ 
+ @param name The name of the tag.
+ @param color The color of the tag.
+ 
+ @return A new instance of Tag.
+ */
 - (instancetype) initWithName:(NSString *)name Color:(NSColor *)color;
 
 @end
